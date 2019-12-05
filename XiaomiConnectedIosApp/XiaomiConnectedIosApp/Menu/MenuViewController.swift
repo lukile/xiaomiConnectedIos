4cb7550185f4ed60b0631eb5eeb948c0de2d8906
Lukile <lukile@macbook-pro-de-under.home>
2019-12-05 20:26:41 +0100

    Added list with the five elements, plus background image

--- XiaomiConnectedIosApp/XiaomiConnectedIosApp/Menu/MenuViewController.swift
+++ XiaomiConnectedIosApp/XiaomiConnectedIosApp/Menu/MenuViewController.swift
@@ -12,20 +12,31 @@ class MenuViewController: UIViewController {
 
     @IBOutlet var menuTableView: UITableView!
     
+    var maxHeight: CGFloat = UIScreen.main.bounds.size.height
+    
     var menu: [Menu] = [Menu]()
     
-    var size = CGSize(width: 40, height: 25)
+    var size = CGSize(width: 50, height: 40)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         createMenuArray()
         self.menuTableView.delegate = self
+        
         self.menuTableView.dataSource = self
+   
     }
 
+    
+    
     func createMenuArray() {
-        menu.append(Menu(featureName: "Cardiac Frequency", featureImage: UIImage(named: "heartbeat")!))
-        menu.append(Menu(featureName: "Pedometer", featureImage: UIImage(named: "foot")!))
+        menu.append(Menu(featureName: "Cardiac frequency", featureImage: UIImage(named: "heartbeat")  ?? UIImage(named: "default")!))
+        menu.append(Menu(featureName: "Pedometer", featureImage: UIImage(named: "foot") ?? UIImage(named: "default")!))
+        menu.append(Menu(featureName: "Battery life", featureImage: UIImage(named: "batteryLife") ?? UIImage(named: "default")!))
+        menu.append(Menu(featureName: "Accelerometer datas", featureImage: UIImage(named: "accelerometer") ?? UIImage(named: "default")!))
+        menu.append(Menu(featureName: "Calorie tracking", featureImage: UIImage(named: "calorie") ?? UIImage(named: "default")!))
+ 
+       
     }
     
     func resizeImage(newSize: CGSize, image: UIImage) -> UIImage {
@@ -38,10 +49,12 @@ class MenuViewController: UIViewController {
         UIGraphicsEndImageContext()
         return newImage
     }
+ 
 }
 
 extension MenuViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
         return self.menu.count
     }
     
@@ -51,10 +64,22 @@ extension MenuViewController: UITableViewDataSource {
    
         cell.textLabel?.text = item.featureName
         cell.imageView!.image = resizeImage(newSize: size, image: item.featureImage)
+       
+        sizeTableView(menuTableView: self.menuTableView, menu: self.menu)
+        
         return cell
     }
     
     
+}
+
+func sizeTableView(menuTableView: UITableView, menu: [Menu]) {
+    var height: Int = Int(menuTableView.rowHeight)
+    height *= menu.count
+    
+    var tableFrame: CGRect = menuTableView.frame
+    tableFrame.size.height = CGFloat(height)
+    menuTableView.frame = tableFrame
 }
 
 extension MenuViewController: UITableViewDelegate {
